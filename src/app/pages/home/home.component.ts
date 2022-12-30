b1a9be4eefcdce51fcb09fa1060998334eeb5c27
Leonel Huachaca <leonel.huachacav@gmail.com>
2022-12-30 10:29:26 -0500

    add design form and user table

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+interface user {
+  nombre: string;
+  apellido: string;
+  edad: number;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  users: user[] = [
+    {
+      nombre: 'Coco',
+      apellido: 'Izquierdo',
+      edad: 35,
+    },
+    {
+      nombre: 'Paul',
+      apellido: 'Hernandez',
+      edad: 12,
+    },
+  ];
 
+  agregarUsuario(nombre: string, apellido: string, edad: number) {}
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 
-
-
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule],
+  exports: [HomeComponent],
 })
-export class PagesModule { }
+export class PagesModule {}
